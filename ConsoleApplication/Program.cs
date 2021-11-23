1ab4267855efc38d14215aa2513094f6ef58442f
m0rk4 <mark_putyato@mail.ru>
2021-11-23 23:58:22 +0300

    Added console tests generation sample

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ConsoleApplication.Entities;
+using ConsoleApplication.Services.Implementations;
+using Core.Services.Implementations;
+
+namespace ConsoleApplication
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            var testsCreator = new TestsCreator();
+            var testsGenerator = new TestsGenerator(testsCreator, new TestsGeneratorConfiguration(
+                "C:\\Users\\Mark\\RiderProjects\\MPP-4\\Tests\\",
+                5,
+                5,
+                5)
+            );
+
+            await testsGenerator.Generate(new List<string>
+                {
+                    "C:\\Users\\Mark\\RiderProjects\\MPP-4\\ConsoleApplication\\Program.cs",
+                    "C:\\Users\\Mark\\RiderProjects\\MPP-4\\Core\\Services\\Implementations\\TestsCreator.cs"
+                }
+            );
+        }
+    }
+}
